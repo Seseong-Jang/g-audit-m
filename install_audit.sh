7c58ad6aaca56a974bb7b36a05150df83760e713
Seseong-Jang <genesis1343@nate.com>
2015-10-01 13:35:23 +0900

    fix script

--- install_audit.sh
+++ install_audit.sh
@@ -7,7 +7,7 @@ if [ $# -ne 1 ]; then
 fi
 
 PW=$1
-PLUGIN_DIR=`mysql -uroot -p${PW} -N -e"show variables like 'plugin_dir'" | grep mysql | awk '{print $2}'`
+PLUGIN_DIR=`mysql -uroot -p${PW} --max_allowed_packet=16M -N -e"show variables like 'plugin_dir'" | grep mysql | awk '{print $2}'`
 
 chown root.root *
 cp -f gm_connect_log.so ${PLUGIN_DIR}
@@ -15,9 +15,9 @@ cp -f gm_connect_log.so ${PLUGIN_DIR}
 mkdir -p /backup/audit/
 touch /backup/audit/audit.log
 
-mysql -uroot -p${PW} -e "INSTALL PLUGIN SERVER_AUDIT SONAME 'gm_connect_log.so';"
-mysql -uroot -p${PW} -e "SET GLOBAL server_audit_logging=ON;"
-mysql -uroot -p${PW} -e "SET GLOBAL server_audit_events=connect;"
-mysql -uroot -p${PW} -e "SET GLOBAL server_audit_file_path = /backup/audit/audit.log;"
+mysql -uroot -p${PW} --max_allowed_packet=16M -e "INSTALL PLUGIN SERVER_AUDIT SONAME 'gm_connect_log.so';"
+mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_logging=ON;"
+mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_events=connect;"
+mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_file_path = /backup/audit/audit.log;"
 
 echo "======== gm connect logging installed. ======="
