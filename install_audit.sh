6b22d9959ac646e4cdec1680f4ea8172ab2ac009
Seseong-Jang <genesis1343@nate.com>
2015-10-01 11:06:50 +0900

    fix script

--- install_audit.sh
+++ install_audit.sh
@@ -12,12 +12,12 @@ PLUGIN_DIR=`mysql -uroot -p${PW} -N -e"show variables like 'plugin_dir'" | grep 
 chown root.root *
 cp -f gm_connect_log.so /usr/lib/mysql/plugin/
 
-mkdir -p /backup/log/
-touch /backup/log/audit.log
+mkdir -p /backup/audit/
+touch /backup/audit/audit.log
 
 mysql -uroot -p${PW} -e "INSTALL PLUGIN SERVER_AUDIT SONAME 'gm_connect_log.so';"
 mysql -uroot -p${PW} -e "SET GLOBAL server_audit_logging=ON;"
 mysql -uroot -p${PW} -e "SET GLOBAL server_audit_events=connect;"
-mysql -uroot -p${PW} -e "SET GLOBAL server_audit_file_path = /backup/log/audit.log;"
+mysql -uroot -p${PW} -e "SET GLOBAL server_audit_file_path = /backup/audit/audit.log;"
 
 echo "======== gm connect logging installed. ======="
