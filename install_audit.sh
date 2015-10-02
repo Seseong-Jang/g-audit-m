be2e4e58b054da4f81501eed2fbdd5ad72bec609
Seseong-Jang <genesis1343@nate.com>
2015-10-02 07:54:33 +0000

    bug fix: install script

--- install_audit.sh
+++ install_audit.sh
@@ -36,8 +36,8 @@ mkdir -p /backup/audit/
 #echo "default folder and file create"
 
 mysql -uroot -p${PW} --max_allowed_packet=16M -e "INSTALL PLUGIN SERVER_AUDIT SONAME 'gm_connect_log.so';"
-mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_logging=ON;"
 mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_events=connect;"
+mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_logging=ON;"
 mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_file_rotate_size=10000000;"
 #mysql -uroot -p${PW} --max_allowed_packet=16M -e "SET GLOBAL server_audit_file_path = '/backup/audit/audit.log';"
 
